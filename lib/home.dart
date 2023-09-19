4bddf65b450e27a42bdf5fa1e8fa99ce26c46bd8
Vishnu8991 <vishnups8991@gmail.com>
2023-09-19 22:33:02 +0530

    Commit

--- lib/home.dart
+++ lib/home.dart
@@ -8,40 +8,42 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            Bar(),
-            Field(),
-
-            SizedBox(height: 30,),
-            
-            Row(
-              children: [
-                Text("Popular Places",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),),
-                Spacer(),
-                Text("View All",style: TextStyle(color: Colors.grey[700], fontSize: 16, fontWeight: FontWeight.bold),),
-
-            SizedBox(height: 40,),
-
-              ],
-            ),
-            GridB()
-
-          ],
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: ElevatedButton(onPressed: (){},
-        style: ElevatedButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))), 
-        child: Padding(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        body: Padding(
           padding: const EdgeInsets.all(12.0),
-          child: Text("Explore Now"),
-        )),
+          child: Column(
+            children: [
+              Bar(),
+              Field(),
+    
+              SizedBox(height: 30,),
+              
+              Row(
+                children: [
+                  Text("Popular Places",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),),
+                  Spacer(),
+                  Text("View All",style: TextStyle(color: Colors.grey[700], fontSize: 16, fontWeight: FontWeight.bold),),
+    
+              SizedBox(height: 40,),
+    
+                ],
+              ),
+              GridB()
+    
+            ],
+          ),
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: ElevatedButton(onPressed: (){},
+          style: ElevatedButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))), 
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text("Explore Now"),
+          )),
+        ),
       ),
     );
   }
--- lib/tour.dart
+++ lib/tour.dart
@@ -14,84 +14,88 @@ class Tour extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.grey[100],
-      body: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            About(),
-
-            SizedBox(height: 18,),
-            
-            Text("About ITC Hotel",style: GoogleFonts.heebo(textStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),)),
-            
-            SizedBox(height: 12,),
-            
-            Text(
-              '''At Welcomhotel by ITC Hotels, Richmond Road, Bengaluru you will find a multi-cuisine restaurant, WelcomCafe Jacaranda, which serves Breakfast, Lunch, and Dinner. The Mahjong Room offers Chinese cuisine for lunch and dinner daily..''',style: TextStyle(height: 1.5,color: Colors.grey[600], fontWeight: FontWeight.bold, fontSize: 14),
-            ),
-            
-            SizedBox(height: 28,),
-
-            Text("Special Facilities",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),),
-
-            SizedBox(height: 18,),
-
-            Facilities(),
-
-            SizedBox(height: 22,),
-
-            Container(padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.network("https://cf.bstatic.com/xdata/images/hotel/max1024x768/31104741.jpg?k=06fb333155038d7ab6c571bda59aa32d313b5764e3ca59befa9ac932440fcad7&o=&hp=1")),
-            ),
-
-            SizedBox(height: 22,),
-
-            Text("Availability",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
-
-            SizedBox(height: 15,),
-
-            Container(
-              child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Container(child: Text("Club"),padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(color: Colors.yellow,),),
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Container(child: Text("Deluxe"),padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(color: Colors.yellow,),),
-                  ),
-                  
-                  Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Container(child: Text("Superior"),padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(color: Colors.yellow,),),
-                  ),
-                  
-                  Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Container(child: Text("Suite"),padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(color: Colors.yellow),),
-                  ),
-                ],
+    return SafeArea(
+      child: Scaffold(backgroundColor: Colors.grey[100],
+        body: Padding(
+          padding: const EdgeInsets.only(bottom: 10, left: 15, right: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              About(),
+    
+              SizedBox(height: 2,),
+              
+              Text("About ITC Hotel",style: GoogleFonts.heebo(textStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),)),
+              
+              SizedBox(height: 5,),
+              
+              Text(
+                '''At Welcomhotel by ITC Hotels, Richmond Road, Bengaluru you will find a multi-cuisine restaurant, WelcomCafe Jacaranda, which serves Breakfast, Lunch, and Dinner.''',style: TextStyle(height: 1.5,color: Colors.grey[600], fontWeight: FontWeight.bold, fontSize: 14),
               ),
-            ),
-          ],
+              
+              SizedBox(height: 15,),
+    
+              Text("Special Facilities",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),),
+    
+              SizedBox(height: 8),
+    
+              Facilities(),
+    
+              SizedBox(height: 8,),
+    
+              Center(
+                child: Container(padding: EdgeInsets.all(5),height: 210,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network("https://cf.bstatic.com/xdata/images/hotel/max1024x768/31104741.jpg?k=06fb333155038d7ab6c571bda59aa32d313b5764e3ca59befa9ac932440fcad7&o=&hp=1")),
+                ),
+              ),
+    
+              SizedBox(height: 10,),
+    
+              Text("Availability",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
+    
+              SizedBox(height: 10,),
+    
+              Container(
+                child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Container(child: Text("Club"),padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(color: Colors.yellow,),),
+                    ),
+    
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Container(child: Text("Deluxe"),padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(color: Colors.yellow,),),
+                    ),
+                    
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Container(child: Text("Superior"),padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(color: Colors.yellow,),),
+                    ),
+                    
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Container(child: Text("Suite"),padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(color: Colors.yellow),),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(bottom: 12, left: 15, right: 15),
-        child: ElevatedButton(onPressed: (){}, 
-        style: ElevatedButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
-        child: Text("View More")),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.only(bottom: 8, left: 15, right: 15),
+          child: ElevatedButton(onPressed: (){}, 
+          style: ElevatedButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
+          child: Text("View More")),
+        ),
       ),
     );
   }
